a8946e281c3c9af49d3a6c99dd5a830c640545d0
Zhonghe-packing <zwcchatgpt2@outlook.com>
2026-09-07 11:22:20 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+window.SUPABASE_URL = 'https://vynzkvabqeawajblbdyp.supabase.co';
+window.SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_bZ9OGGb4afQ8gCmv5Ogvbg_7rr0Y7Ak';
+window.SUPABASE_BUCKET = 'zhonghe-media';
